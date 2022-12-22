4fb3e0266014755e7f0c2e120439940ee88dd11d
ваше имя <dimarik-tixonov@yandex.ru>
2022-12-22 22:17:52 +0300

    add: добавит шаблонную разметку карточки товара

--- src/components/products-list/products-list.tsx
+++ src/components/products-list/products-list.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Product from '../product/product';
 import styles from './product-list.module.scss';
 
 export default function ProductList(): JSX.Element {
   return (
     <div className={styles.products}>
-      <p>Товар</p>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
     </div>
   );
 }
