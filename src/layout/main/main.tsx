2e3425d3097d089ba6ad57582ac26e70458b9227
ваше имя <dimarik-tixonov@yandex.ru>
2022-12-23 20:11:10 +0300

    add: добавит утилитарную функцию для преобразования данных с сервера

--- src/layout/main/main.tsx
+++ src/layout/main/main.tsx
@@ -2,24 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { Preloader } from '../../components/preloader/preloader';
 import ProductList from '../../components/products-list/products-list';
 import { API_KEY, API_URL } from '../../config';
+import { IBackProduct } from '../../types';
+import { transformProduct } from './helpers';
 
 export function Main(): JSX.Element {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  // TODO типизировать заголовок
   useEffect(() => {
     fetch(API_URL, {
       headers: {
-        Authorization: API_KEY as string,
+        Authorization: API_KEY,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        if (data.shop) {
-          setProducts(data.shop);
+        if (data.featured) {
+          setProducts(data.featured.map(transformProduct));
         }
-
+      })
+      .catch((error) => {
+        throw (error);
+      })
+      .finally(() => {
         setIsLoading(false);
       });
   }, []);
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface IBackProduct {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    icon: string;
+}
+
+export interface IProduct {
+  id: string,
+  name: string,
+  description: string,
+  price: number,
+  image: string,
+}
