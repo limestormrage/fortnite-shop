06713b652e2d7ce17652cc292b75f0267d6477a8
ваше имя <dimarik-tixonov@yandex.ru>
2022-12-24 23:02:26 +0300

    add: добавит разметку компонента корзины

---
+++ src/components/card/card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cn from 'classnames';
+import styles from './card.module.scss';
+
+export default function Cart(): JSX.Element {
+  return (
+    <div className={cn(styles.cart, 'deep-purple darken-4 white-text')}>
+      <i className="material-icons">shopping_cart</i>
+      <span>0</span>
+    </div>
+  );
+}
--- src/layout/main/main.tsx
+++ src/layout/main/main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cart from '../../components/card/card';
 import { Preloader } from '../../components/preloader/preloader';
 import ProductList from '../../components/products-list/products-list';
 import { useFetchProducts } from './useFetchProducts';
@@ -10,6 +11,7 @@ export function Main(): JSX.Element {
 
   return (
     <main className="container main">
+      <Cart />
       {isLoading && <Preloader />}
       {isError && (
         <div>
